fe044af6953fa04d6f7d2d2e6a9654df95f31d03
Nazariy Tivirikin <n.tivirikin@queensu.ca>
2019-02-27 17:29:13 -0500

    Rework get_by_id into get_recordings and add tests

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 from urllib import request
-from xenocanto import get_by_id
+from xenocanto import get_recordings
 import os
 
 class TestCases(unittest.TestCase):
@@ -9,18 +9,13 @@ class TestCases(unittest.TestCase):
         status = request.urlopen('https://www.xeno-canto.org/api/2/recordings?query=cnt:any').getcode()
         self.assertEqual(status, 200)
 
-    
-    # Check if file was retrieved with correct catalog number
-    def test_single_get_by_id(self):
-        cat_id = get_by_id(438595)
-        self.assertTrue(os.path.isfile('Blue_Jay438595.mp3'))
-        self.assertEqual(cat_id, ['438595'])
+    # Check if query can be made with one piece of criteria
+    def test_query_single(self):
+        get_recordings(['gen:Otis'])
+        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:Otis.json'))
+        self.assertTrue(os.path.isfile(os.getcwd() + '/recordings/Great_Bustard459281.mp3'))
 
-
-    # Check if files falling in the catalog number range provided have been downloaded with correct catalog numbers
-    def test_range_get_by_id(self):
-        cat_id = get_by_id(438595, 438596)
-        self.assertTrue(os.path.isfile('Blue_Jay438595.mp3'))
-        self.assertTrue(os.path.isfile('Kamchatka_Leaf_Warbler438596.mp3'))
-        self.assertEquals(cat_id[0], '438595')
-        self.assertEquals(cat_id[1], '438596')
+    # Check if query can be made with multiple criteria
+    def test_query_multiple(self):
+        get_recordings(['gen:rhea', 'cnt:Brazil'])
+        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:rhea_cnt:Brazil.json'))
--- xenocanto.py
+++ xenocanto.py
@@ -1,28 +1,33 @@
 # Simplify acquisition of recordings from xeno-canto.org based on desired criteria
+from urllib import request
 import json
 import os
-from urllib import request
 
-# Grab recording based on catalog number range (or singele)
-def get_by_id(start=None, end=None):
-    if (end==None):
-        url = 'https://www.xeno-canto.org/api/2/recordings?query=nr:' + str(start)
-    else:
-        url = 'https://www.xeno-canto.org/api/2/recordings?query=nr:' + str(start) + '-' + str(end)
-    
+
+def get_recordings(list_in):
+    if (os.path.isdir(os.getcwd() + '/queries')==False):
+        os.mkdir(os.getcwd() + '/queries')
+    if (os.path.isdir(os.getcwd() + '/recordings')==False):
+        os.mkdir(os.getcwd() + '/recordings')
+    url = 'https://www.xeno-canto.org/api/2/recordings?query='
+    for i in range(0, len(list_in)):
+        url += (list_in[i] + '%20')
     response = request.urlopen(url)
     data = response.read()
     data_json = json.loads(data.decode('utf-8'))
-    result = []
+    query_info = open(os.getcwd() + '/queries/temp.txt', 'w+')
+    json.dump(data_json, query_info)
+    query_info.close()
+    filename = url[50:-1]
+    os.rename(os.getcwd() + '/queries/temp.txt', os.getcwd() + '/queries/' + (filename.replace('%20', '_')).replace('%2', '') + '.json')
 
     for i in range(0, len(data_json["recordings"])):
-        download_url = 'https:' + data_json["recordings"][i]["file"]
-        mp3 = open('temp.mp3', 'wb')
-        dl = request.urlopen(download_url)
-        data = dl.read()
-        mp3.write(data)
-        mp3.close()
         cat_id = data_json["recordings"][i]["id"]
-        result.append(cat_id)
-        os.rename('temp.mp3', (data_json["recordings"][i]["en"]).replace(' ', '_') + cat_id + '.mp3')
-    return result
+        if (os.path.isfile(os.getcwd() + '/recordings/' + (data_json["recordings"][i]["en"]).replace(' ', '_') + cat_id + '.mp3')) == False:
+            download_url = 'https:' + data_json["recordings"][i]["file"]
+            mp3 = open(os.getcwd() + '/recordings/temp.mp3', 'wb')
+            dl = request.urlopen(download_url)
+            data = dl.read()
+            mp3.write(data)
+            mp3.close()
+            os.rename(os.getcwd() + '/recordings/temp.mp3', os.getcwd() + '/recordings/' + (data_json["recordings"][i]["en"]).replace(' ', '_') + cat_id + '.mp3')
