60f3de83d7a7c677821b558fe93504cc58469953
Nazariy Tivirikin <n.tivirikin@queensu.ca>
2019-03-01 12:30:19 -0500

    Fix downloading only from first page; refactor everything

--- test.py
+++ test.py
@@ -12,10 +12,10 @@ class TestCases(unittest.TestCase):
     # Check if query can be made with one piece of criteria
     def test_query_single(self):
         get_recordings(['gen:Otis'])
-        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:Otis.json'))
-        self.assertTrue(os.path.isfile(os.getcwd() + '/recordings/Great_Bustard459281.mp3'))
+        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:Otis/page1.json'))
+        self.assertTrue(os.path.isfile(os.getcwd() + '/recordings/GreatBustard459281.mp3'))
 
     # Check if query can be made with multiple criteria
     def test_query_multiple(self):
         get_recordings(['gen:rhea', 'cnt:Brazil'])
-        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:rhea_cnt:Brazil.json'))
+        self.assertTrue(os.path.isfile(os.getcwd() + '/queries/gen:rhea_cnt:Brazil/page1.json'))
--- xenocanto.py
+++ xenocanto.py
@@ -1,33 +1,47 @@
-# Simplify acquisition of recordings from xeno-canto.org based on desired criteria
 from urllib import request
-import json
-import os
+import json, os, errno
+
+def create_dir(directory):
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
 
 
-def get_recordings(list_in):
-    if (os.path.isdir(os.getcwd() + '/queries')==False):
-        os.mkdir(os.getcwd() + '/queries')
-    if (os.path.isdir(os.getcwd() + '/recordings')==False):
-        os.mkdir(os.getcwd() + '/recordings')
+def get_recordings(search):
+    create_dir(os.getcwd() + '/queries')
+    create_dir(os.getcwd() + '/recordings')
     url = 'https://www.xeno-canto.org/api/2/recordings?query='
-    for i in range(0, len(list_in)):
-        url += (list_in[i] + '%20')
-    response = request.urlopen(url)
-    data = response.read()
+    for val in search:
+        url += val + '%20'
+    r = request.urlopen(url)
+    data = r.read()
     data_json = json.loads(data.decode('utf-8'))
-    query_info = open(os.getcwd() + '/queries/temp.txt', 'w+')
-    json.dump(data_json, query_info)
-    query_info.close()
-    filename = url[50:-1]
-    os.rename(os.getcwd() + '/queries/temp.txt', os.getcwd() + '/queries/' + (filename.replace('%20', '_')).replace('%2', '') + '.json')
+    pages = data_json["numPages"]
+    name = ((url[50:-1]).replace('%20', '_')).replace('%2', '')
+    create_dir(os.getcwd() + '/queries/' + name)
 
-    for i in range(0, len(data_json["recordings"])):
-        cat_id = data_json["recordings"][i]["id"]
-        if (os.path.isfile(os.getcwd() + '/recordings/' + (data_json["recordings"][i]["en"]).replace(' ', '_') + cat_id + '.mp3')) == False:
-            download_url = 'https:' + data_json["recordings"][i]["file"]
-            mp3 = open(os.getcwd() + '/recordings/temp.mp3', 'wb')
-            dl = request.urlopen(download_url)
-            data = dl.read()
-            mp3.write(data)
-            mp3.close()
-            os.rename(os.getcwd() + '/recordings/temp.mp3', os.getcwd() + '/recordings/' + (data_json["recordings"][i]["en"]).replace(' ', '_') + cat_id + '.mp3')
+    for i in range(1, pages + 1):
+        url = 'https://www.xeno-canto.org/api/2/recordings?query='
+        for val in search:
+            url += val + '%20'
+        url += '&page=' + str(i)
+        r = request.urlopen(url)
+        data = r.read()
+        data_json = json.loads(data.decode('utf-8'))
+        query_info = open(os.getcwd() + '/queries/' + name + '/temp.txt', 'w+')
+        json.dump(data_json, query_info)
+        query_info.close()
+        os.rename(os.getcwd() + '/queries/' + name + '/temp.txt', os.getcwd() + '/queries/' + name + '/page' + str(i) + '.json')
+
+        for k in range(0, len(data_json["recordings"])):
+            rec_name = (data_json["recordings"][k]["en"]).replace(' ', '') + data_json["recordings"][k]["id"]
+            if (os.path.isfile(os.getcwd() + '/recordings/' + rec_name + '.mp3')) == False:
+                download_url = 'https:' + data_json["recordings"][k]["file"]
+                mp3 = open(os.getcwd() + '/recordings/temp.mp3', 'wb')
+                dl = request.urlopen(download_url)
+                data = dl.read()
+                mp3.write(data)
+                mp3.close()
+                os.rename(os.getcwd() + '/recordings/temp.mp3', os.getcwd() + '/recordings/' + rec_name + '.mp3')
