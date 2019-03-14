b6bccafb9489bd9f3c9e49dcaecfdaaae5affcc4
Nazariy Tivirikin <n.tivirikin@queensu.ca>
2019-03-13 23:08:27 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xeno-canto',
-    version='1.0.0',
+    version='1.0.1',
     description='xeno-canto.org API Wrapper',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
