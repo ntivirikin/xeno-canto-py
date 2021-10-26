f32a02ab1de5c5affaa1889633dee016864444e8
Nazariy Tivirikin <n.tivirikin@queensu.ca>
2021-10-26 14:50:31 -0400

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xeno-canto',
-    version='2.0',
+    version='2.0.2',
     description='xeno-canto.org API Wrapper',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
